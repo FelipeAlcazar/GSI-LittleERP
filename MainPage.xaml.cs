4fdb7aaf52264805246b5d9d3416e87ae28f83fb
FelipeAlcazar <felipe.alcazar@alu.uclm.es>
2024-04-01 14:50:38 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace LittleERP
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private MySqlConnection connection;
+        private ObservableCollection<Gasto> expenses;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            InitializeDatabase();
+        }
+
+
+        private void InitializeDatabase()
+        {
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void RegisterButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Persistencia/GestorBaseDatos.cs
@@ -0,0 +1,81 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LittleERP.Persistencia
+{
+    public class GestorBaseDatos
+    {
+        private MySqlConnection connection;
+        string databaseFilePath = @"C:\path\to\your\databasefile.mysql";
+        string usuario = "root";
+        string contraseña = "pass";
+
+        public GestorBaseDatos(string servidor, string usuario, string contraseña, string baseDeDatos)
+        {
+            string cadenaConexion = $"server=localhost;user={usuario};password={contraseña};database={databaseFilePath}";
+            connection = new MySqlConnection(cadenaConexion);
+        }
+
+        public ObservableCollection<Gasto> ObtenerGastos()
+        {
+            ObservableCollection<Gasto> gastos = new ObservableCollection<Gasto>();
+
+            try
+            {
+                connection.Open();
+
+                string consulta = "SELECT * FROM gastos";
+                MySqlCommand comando = new MySqlCommand(consulta, connection);
+
+                using (MySqlDataReader lector = comando.ExecuteReader())
+                {
+                    while (lector.Read())
+                    {
+                        gastos.Add(new Gasto
+                        {
+                            id = lector.GetInt32(0),
+                            cantidad = lector.GetDouble(1),
+                            descripcion = lector.GetString(2),
+                            fecha = lector.GetDateTime(3)
+                        });
+                    }
+                }
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Manejar excepciones
+            }
+
+            return gastos;
+        }
+
+        public void AgregarGasto(double monto, string descripcion, DateTime fecha)
+        {
+            try
+            {
+                connection.Open();
+
+                string consulta = "INSERT INTO gastos (monto, descripcion, fecha) VALUES (@monto, @descripcion, @fecha)";
+                MySqlCommand comando = new MySqlCommand(consulta, connection);
+                comando.Parameters.AddWithValue("@monto", monto);
+                comando.Parameters.AddWithValue("@descripcion", descripcion);
+                comando.Parameters.AddWithValue("@fecha", fecha);
+
+                comando.ExecuteNonQuery();
+
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                // Manejar excepciones
+            }
+        }
+    }
+}
